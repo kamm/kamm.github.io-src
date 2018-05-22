2333d6f5bc156612c2d982706c6717bdd2c24b0f
Kamil Mętrak <kamil.metrak@gmail.com>
2018-05-22 11:42:29 +0200

    rebuilding site Tue, May 22, 2018 11:42:29 AM

--- push.sh
+++ push.sh
@@ -1,4 +1,20 @@
-git add .
-git commit -m "Updating site" && git push origin master
-git subtree push --prefix=public https://github.com/kamm/kamm.github.io gh-pages
+#!/bin/bash
 
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project.
+hugo
+
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+      then msg="$1"
+fi
+git commit -m "$msg"
+
+      # Push source and build repos.
+git push origin master
+git subtree push --prefix=public https://github.com/kamm/kamm.github.io master
